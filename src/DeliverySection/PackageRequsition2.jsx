2bc592cd6da3d0020cd97a1f9912bca298a89b83
NuskaFayik <mffnuska@gmail.com>
2023-03-24 13:13:32 +0530

    Displayed Package Details

--- src/DeliverySection/PackageRequsition2.jsx
+++ src/DeliverySection/PackageRequsition2.jsx
@@ -1,38 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Box2 from './Box2';
 import Button from './Button';
 
 
 const PackageRequsition2 = () => {
+  const[data,setData]=useState([]);
+  useEffect(() => {
+    fetch("http://localhost:8080/PackageRequisition/package-count")
+      .then((response) => response.json())
+      .then((jsondata) => setData(jsondata));
+  }, []);
+
   return (
     <React.Fragment>
-      <div className="d-flex align-items-center justify-content-center" >
+      <div classNameName="d-flex align-items-center justify-content-center" >
          <div style={{ fontSize: '30px', fontWeight: 'bold' }}>Package Needed</div>
       </div>
-
-      <div class="row">
-          <div class="col-sm-6">
-          <Box2 text=" Type A : Packages 10" />
+      {Object.entries(data).map(([key, value])=>(
+          <div className="col-sm-6 d-flex justify-content-center" key={key}>
+              <Box2 text={`Type ${key} :${value}`} />
           </div>
-
-          <div class="col-sm-6">
-          <Box2 text=" Type B : Packages 10" />
-          </div>
-      </div>
-
-      <div class="row">
-          <div class="col-sm-6">
-          <Box2 text=" Type C : Packages 10" />
-          </div>
-      </div>
-
-      <div class="d-flex justify-content-end align-items-end mb-3">
-         <div class="align-self-end ml-10 "> 
+      )
+      )
+      }
+      <div className="d-flex justify-content-end align-items-end mb-3">
+         <div className="align-self-end ml-10 "> 
          <Button style="margin-top: 20px;" variant="secondary" bg="grey" text="Submit" textColor="dark" />
          </div>
       </div>
-
-
     </React.Fragment>
   );
 };
