a3bed102d3b0ca34428f7a4b4b5681e3b6b257dd
Amal Shalinda <Shalindajda99@gmail.com>
2023-03-28 07:04:50 +0530

    changes before checkout

--- src/Warehouse/AddItems.jsx
+++ src/Warehouse/AddItems.jsx
@@ -2,17 +2,61 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const AddItems = () => {
+  const [values, setValues] = useState([]);
   const [itemData, setItemData] = useState([]);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const response = await fetch(
+  //       "http://localhost:8080/ItemDetails/viewAllItems"
+  //     );
+  //     const jsonData = await response.json();
+  //     setItemData(jsonData);
+  //     setValues(new Array(itemData.length).fill(''));
+  //   };
+  //   fetchData();
+  // }, []);
+
   useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch(
-        "http://localhost:8080/ItemDetails/viewAllItems"
-      );
-      const jsonData = await response.json();
-      setItemData(jsonData);
-    };
-    fetchData();
+    // Fetch data from the backend when the component mounts
+    fetch("http://localhost:8080/ItemDetails/viewAllItems")
+      .then((response) => response.json())
+      .then((data) => {
+        setItemData(data);
+        const newValues = {};
+        itemData.forEach((item) => {
+          newValues[item.id] = "";
+        });
+        setValues(newValues);
+      })
+      .catch((error) => {
+        console.error("There was a problem with the fetch operation:", error);
+      });
   }, []);
+
+  const handleInputChange = (index, event) => {
+    const newValues = [...values];
+    newValues[index] = event.target.value;
+    setValues(newValues);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Here is where you would send the values to the backend using the Fetch API
+    fetch("https://my-backend.com/my-endpoint", {
+      method: "POST",
+      body: JSON.stringify(values),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        // Handle the response from the backend here
+      })
+      .catch((error) => {
+        console.error("There was a problem with the fetch operation:", error);
+      });
+  };
+
   return (
     <React.Fragment>
       <h1>Add Items</h1>
@@ -35,7 +79,11 @@ const AddItems = () => {
                   <td> {details.id}</td>
                   <td>{details.name}</td>
                   <td>
-                    <input type="number" />
+                    <input
+                      type="number"
+                      value={values[details.id]}
+                      onChange={(event) => handleInputChange(details.id, event)}
+                    />
                   </td>
                 </tr>
               ))}
@@ -49,7 +97,11 @@ const AddItems = () => {
         style={{ zIndex: "11" }}
       >
         <input type="date" className="me-3 p-1" />
-        <button className="btn btn-primary" style={{ width: "150px" }}>
+        <button
+          className="btn btn-primary"
+          style={{ width: "150px" }}
+          onClick={handleSubmit}
+        >
           Save
         </button>
       </div>
