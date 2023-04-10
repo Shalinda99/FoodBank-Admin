546c42f540cadb4e8a3a40cf42fb7f2b98b38bc4
Milanka00 <milankatharangana01@gmail.com>
2023-04-10 12:19:24 +0530

    new routes added

---
+++ src/DeliverySection/collectionSummary.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CollectionSummary = () => {
+    return ( 
+        <React.Fragment>
+            
+        </React.Fragment>
+     );
+}
+ 
+export default CollectionSummary;
---
+++ src/DeliverySection/foodDonations.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FoodDonation = () => {
+    return ( 
+        <React.Fragment>
+            
+        </React.Fragment>
+     );
+}
+ 
+export default FoodDonation;
