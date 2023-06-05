0a0e1e190d47050b4c1f85b64eae7bed45f59cf0
tha-ree-jan <thareejap.20@uom.lk>
2023-06-05 13:38:39 +0530

    merge conflict

--- src/AdminSection/ARoutes.jsx
+++ src/AdminSection/ARoutes.jsx
@@ -20,20 +20,4 @@ const ARoutes = () => {
   );
 };
 
-
-    const location = useLocation();
-    const includePaths = ['/AdminHome','/DataSupplierDetails','/VictimDetails','/AcceptedVictimsDetails','/RejectedVictimsDetails','/userCreation']; // include only these paths to render navbar
-    return ( 
-        <React.Fragment>
-        
-         
-         {includePaths.includes(location.pathname) && <ANavBar />}
-
-
-        
-
-        </React.Fragment>
-     );
-}
- 
 export default ARoutes;
